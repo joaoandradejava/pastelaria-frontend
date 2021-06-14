6dd1188dcc73a7954adbec8b47a8dc6bce06e2de
joaoandradejava <andradepinheironeto24@hotmail.com>
2021-06-14 01:21:32 -0300

    ajeitando

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080
 
-app.use(express.static(__dirname + '/dist/pastelaria-frontend'));
+app.use(express.static(__dirname + '/dist/pastelaria'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(__dirname + '/dist/pastelaria-frontend/index.html'),
+    res.sendFile(__dirname + '/dist/pastelaria/index.html'),
 );
 
 // Start the app by listening on the default Heroku port
